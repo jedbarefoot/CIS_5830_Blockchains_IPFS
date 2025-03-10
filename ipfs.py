a5545ae387a3737eed79241958e63732bb42c169
jedbarefoot <107897225+jedbarefoot@users.noreply.github.com>
2025-03-10 16:10:44 -0400

    Add files via upload

--- ipfs.py
+++ ipfs.py
@@ -4,14 +4,14 @@ import json
 def pin_to_ipfs(data):
 	assert isinstance(data,dict), f"Error pin_to_ipfs expects a dictionary"
 	#YOUR CODE HERE
-	json_data = json.dumps({"pinataContent": data})
-	url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+	json_data = json.dumps(data)
+	url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
 	headers = {
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIxNjUxYmY1Yi1kZjQzLTQyZjMtODkwYy01NmZjOGIwM2VmNjkiLCJlbWFpbCI6ImplZGJhcmVmb290QGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiJjMTI0NWJkMjE0OTU4OWJjM2NlZiIsInNjb3BlZEtleVNlY3JldCI6Ijg0M2FjYjIzNmJmNWM4ZTdhNTIxM2NiMWE4Y2I3YjI5ZDM4ZjZlNzMxZGVhZjUyNjQyN2FkMzdjM2E5MmVjMDAiLCJleHAiOjE3NzMxNjgzMTR9.LwD_DMXnyGxsA_RwDCoX_t6aS_6aygxt1sSCRzeBzz",
 		"Content-Type": "application/json"
     }
 
-	response = requests.post(url, headers=headers, json=json_data)
+	response = requests.post(url, headers=headers, data=json_data)
 
 	cid = response.json().get("IpfsHash")
 
